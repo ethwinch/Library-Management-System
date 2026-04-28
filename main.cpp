985eae9b533a0a30dca4dde21efa4a001fdcd807
finland <ethwinch@gmail.com>
2026-04-27 22:00:06 -0400

    Updated checklist in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,32 @@
 // Library Management System 
-    // track borrowed books
-    // have a waitlist for books
-    // a member record
-    // ability to search books by ISBN
-    // ability to view all books in the catalog
-    // ability to check out books
+    // track borrowed books ✅
+    // have a waitlist for books ✅
+    // a member record ✅
+    // ability to search books by ISBN [ ]
+    // ability to view all books in the catalog [ ]
+    // ability to check out books ✅
 
-// a queue for a waitlist for borrowing books that are currently unavailable
-// a map that can be searched using a string of the book’s ISBN to return the book data type
-// a vector which contains pointers to all the books in the catalogue to make displaying all books easier
+// a queue for a waitlist for borrowing books that are currently unavailable ✅
+// a hash_map that can be searched using a string of the book’s ISBN to return the book data type [ ]
+// a vector which contains pointers to all the books in the catalogue to make displaying all books easier [ ]
 
-// Map:     ACTUALLY SEARCH BY ISBN BUT NOT IN MAP...?
+// Map:     SEARCH BY ISBN
     // KEY - book’s ISBN
-    // VALUE - book class data type
+    // VALUE - book reference
 
 // ALGORITHMS:
-    // a binary search algorithm
+    // ***BEST: Hash map [ ]
+        // * O(n) search
+        // #include <unordered_map>
+        // unordered_map<int, Book*> library;
+        // int = ISBN, Book* = book reference
+
+    // Binary Search [ ]
+        // * O(log n) Search, O(n) insertion
+        // * books must be sorted by ISBN first
         // allow users to search the map for a specific book by ISBN
         // display book info: author, title and ISBN
 
-        // > check out current book
-            // book available?
-                // update books availability
-                // add book to member's "borrowed books"
-            // book not available?
-                // add member to queue
-
-
-        // > return book
-            // book removed from member's "borrowed books" vector
-            // mark book as available
-                // book is borrowed by next person in queue if person exists
 
 
 #include <iostream>
